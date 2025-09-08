ea68e12990b3ebeabdcc7f46160d1f8cb765f9e0
Sanyamdksh <sanyamdaksh19@gmail.com>
2025-09-08 23:35:45 +0530

    fixed state

--- src/App.js
+++ src/App.js
@@ -79,6 +79,7 @@ const App = () => {
 
         {selectedFriend && (
           <FormSplitBill
+            key={selectedFriend.id}
             selected={selectedFriend}
             onSplitBill={handleSplitBill}
           />
